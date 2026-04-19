78d42cc0b3d7ab383247524b504c9cdd1c0f5d60
daulrus <daulrusya@gmail.com>
2026-04-20 03:45:14 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ const Navbar = () => {
     )}>
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="flex items-center gap-3">
-  <img src="/logos/Logoo.png" alt="logo" className="h-10 w-auto" />
+  <img src="/logos/logo.png" alt="logo" className="h-10 w-auto" />
   <span className="text-xl font-semibold">
     Bolds<span className="text-purple-500">.uz</span>
   </span>
